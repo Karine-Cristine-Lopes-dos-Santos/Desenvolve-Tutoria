91b2086b8ca8eb4edd3f11b1d4ca550a037db901
Karine Cristine Lopes dos Santos <112910735+Karine-Cristine-Lopes-dos-Santos@users.noreply.github.com>
2025-07-21 21:21:41 -0300

    Update Tutoria21-07-25.js
    Mudando o código pra uma função

--- Tutoria21-07-25.js
+++ Tutoria21-07-25.js
@@ -1,5 +1,5 @@
-let num1 = 9;
-let num2 = 5;
-
-soma = num1 + num2;
-console.log("A soma é igual à:" + soma);
+function soma (a,b){
+  return a+b;
+}
+ 
+console.log("A soma é igual à:" + soma(9,5));
